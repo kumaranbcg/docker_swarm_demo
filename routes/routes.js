d1527eb1b72ddafecb314f2977f53dc88b6e4490
Kumaran <40437078+kumaranbcg@users.noreply.github.com>
2021-09-03 19:23:15 +0530

    code merge

--- routes/routes.js
+++ routes/routes.js
@@ -3,22 +3,22 @@ const axios = require("axios");
 const router = express.Router();
 router.get("/healthcheck", (err, res) => {
     return res.send(`
-        I am Healthcheck Service 1\n
+        I am Healthcheck Service 2\n
         Node env: ${process.env.NODE_ENV}\n
         DB username: ${process.env.DB_USERNAME}\n
         DB password: ${process.env.DB_PASSWORD}
         `);
 });
 router.get("/", (err, res) => {
-    return res.send("I am Service one");
+    return res.send("I am Service two");
 });
 router.get("/hello", (err, res) => {
-    return res.send("Saying hello one...");
+    return res.send("Saying hello two...");
 });
 router.get("/getService2/healthcheck", async (err, res) => {
     const { data } = await axios({
         method: "GET",
-        url: "http://myservice2:2174/healthcheck",
+        url: "http://myservice1:2173/healthcheck",
     }).catch(() => true);
     return res.send(data);
 });

571091d99bf1806603645d45be5774f233479f68
Kumaran <40437078+kumaranbcg@users.noreply.github.com>
2021-09-04 21:25:13 +0530

    code merge

--- routes/routes.js
+++ routes/routes.js
@@ -15,7 +15,7 @@ router.get("/", (err, res) => {
 router.get("/hello", (err, res) => {
     return res.send("Saying hello two...");
 });
-router.get("/getService2/healthcheck", async (err, res) => {
+router.get("/getService1/healthcheck", async (err, res) => {
     const { data } = await axios({
         method: "GET",
         url: "http://myservice1:2173/healthcheck",
